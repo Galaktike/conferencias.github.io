3ef1053d9de63f688a8447be25aed9277c504881
Galaktike <violindelcielo@gmail.com>
2023-06-13 12:43:38 -0300

    7mo commit cambio por html ticket

--- assets/js/mi_js.js
+++ assets/js/mi_js.js
@@ -4,28 +4,11 @@ function totalAPagar(cantTickets, porcentaje) {
   return resultado;
 }
 
-let comprarTicket = document.getElementsByClassName("comprarTicket");
-let contenedorTicket = document.getElementById("contenedorTicket");
-let presentacion = document.getElementById("presentacion");
-let oradores = document.getElementById("oradores");
-let fecha = document.getElementById("fecha-lugar");
-let form = document.getElementById("form");
-
 let cantTickets = document.getElementById("cantidad");
 let porcentaje = document.getElementById("porcentaje");
 let totalPagar = document.getElementById("totalPagar");
 
 let formulario = document.getElementById("form-ticket");
-
-for (compra of comprarTicket) {
-  compra.addEventListener("click", () => {
-    contenedorTicket.style.display = "block";
-    presentacion.style.display = "none";
-    oradores.style.display = "none";
-    fecha.style.display = "none";
-    form.style.display = "none";
-  });
-}
 
 formulario.addEventListener("submit", (evento) => {
   evento.preventDefault();
